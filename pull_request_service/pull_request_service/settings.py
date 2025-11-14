2d2b9d233a7c239eb49fd796d98da001cdc45d56
Михаил Астанин <misha100904@gmail.com>
2025-11-14 15:53:53 +0300

    Add docker deploy

--- pull_request_service/pull_request_service/settings.py
+++ pull_request_service/pull_request_service/settings.py
@@ -63,11 +63,14 @@ WSGI_APPLICATION = 'pull_request_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django_user',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 
